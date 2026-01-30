59a5f3e8db9b954a365621f6cac540c40a85d39b
Rio_0909 <pssharma0098@gmail.com>
2026-01-30 11:50:00 +0000

    cg

--- Assets/Scripts/DialogueChoice.cs
+++ Assets/Scripts/DialogueChoice.cs
@@ -1,6 +1,14 @@
-using UnityEngine;
+using System;
 
+[Serializable]
 public class DialogueChoice
 {
-    
+    public string text;
+    public Action onSelect;
+
+    public DialogueChoice(string text, Action onSelect)
+    {
+        this.text = text;
+        this.onSelect = onSelect;
+    }
 }
